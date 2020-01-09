20c6ff1bea0e6e18c78e9162dd94d8fcbb79afe7
Jonathan Krall Olsson <jonathan_olsson88@hotmail.com>
2020-01-09 09:45:32 +0100

    8 fix 2.0

--- src/Quiz.js
+++ src/Quiz.js
@@ -6,7 +6,8 @@ class Quiz extends Component
 {
 render()
 {
-  return <div><div className=QuizQuestion</div></div>
+  return <div><div className="QuizQuestion">{quizData.quiz_questions[0]
+  .instruction_text}</div></div>
 }
 constructor(props)
 {
