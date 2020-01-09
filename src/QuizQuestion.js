4d405c1ca3bdb7919ebbc674326bf57af3c6b4c4
Jonathan Krall Olsson <jonathan_olsson88@hotmail.com>
2020-01-09 11:33:37 +0100

    Module 7

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -8,6 +8,10 @@ class QuizQuestion extends Component
     if(buttonText === this.props.quiz_question.answer)
     {
       this.props.showNextQuestionHandler()
+      this.setState({incorrectAnswer:false} )
+    }
+    else {
+      this.setState({incorrectAnswer:true})
     }
   }
   render()
@@ -25,9 +29,14 @@ class QuizQuestion extends Component
           })}
           </ul>
         </section>
+        {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right</p>:null}
       </main>
     )
-
+  }
+  constructor(props)
+  {
+    super(props)
+    this.state= {incorrectAnswer: false}
   }
 }
 
