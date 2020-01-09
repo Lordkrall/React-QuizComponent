4410c52dec040dcda036e16d209d03f37242c4d1
Jonathan Krall Olsson <jonathan_olsson88@hotmail.com>
2020-01-09 11:49:34 +0100

    Fixed module 8.2

--- src/Quiz.js
+++ src/Quiz.js
@@ -6,11 +6,15 @@ let quizData = require('./quiz_data.json')
 
 class Quiz extends Component
 {
+  handleResetClick()
+  {
+this.setState({quiz_position:1})
+  }
 render()
 {
   const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
   return <div>
-  {isQuizEnd ?<QuizEnd resetClickHandle ={this.handleResetClick.bind(this)} /> :
+  {isQuizEnd ?<QuizEnd resetClickHandler ={this.handleResetClick.bind(this)} /> :
   <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions
     [this.state.quiz_position - 1]} />}
     </div>
@@ -25,10 +29,6 @@ constructor(props)
 {
   super(props)
   this.state = {quiz_position: 1}
-}
-handleResetClick()
-{
-this.setState({quiz_position:1})
 }
 }
 
